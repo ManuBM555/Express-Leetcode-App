661b472db90ca9291e8f6c31215a2566606c1f13
Manu B M <bmmanu555@gmail.com>
2023-05-10 23:26:37 +0530

    Signup and Login
    Both routes working

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(express.json());
 const port = 3001
 
-const USERS = [];
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+const USERS = [
+  {
+    "email": "manu@gmail.com",
+    "password": "manu1",
+    "role": "admin"
+  },
+  {
+    "email": "charan@gmail.com",
+    "password": "charan1",
+    "role": "user"
+  },
+  {
+    "email": "pratap@gmail.com",
+    "password": "pratap1",
+    "role": "user"
+  }
+];
 
 const QUESTIONS = [{
     title: "Two states",
@@ -18,16 +41,44 @@ const SUBMISSION = [
 
 ]
 
+app.get('/',function(req, res) {
+  res.send('Home Page');
+})
+
+// SignUP
 app.post('/signup', function(req, res) {
+  
   // Add logic to decode body
   // body should have email and password
 
-
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
-
   // return back 200 status code to the client
-  res.send('Hello World!')
+
+  
+
+  const { email, password, role} = req.body
+  // console.log(req.body);
+  if(!email || !password || !role) {
+    res.status(400).send('enter email and password')
+  }
+
+  const UserExist = USERS.find((user) => user.email === email);
+
+  if(UserExist) {
+    return res.status(400)
+    .json({
+      message:'User already exist'})
+  }
+  else {
+    const newUSer = req.body;
+    USERS.push(newUSer)
+    res.send('SignUp successfull')
+  }
+})
+
+app.get('/signup', function(req,res) {
+  res.json(USERS)
 })
 
 app.post('/login', function(req, res) {
@@ -41,9 +92,22 @@ app.post('/login', function(req, res) {
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
+  
+  const { email, password } = req.body;
 
+  if(!email || !password) {
+    res.status(400).send('enter email and password')
+  }
 
-  res.send('Hello World from route 2!')
+  const UserExist = USERS.find((user) => user.email === email && user.password === password);
+  
+  if(UserExist) {
+    const token = 'You are now logged in';
+    return res.status(200).send(token)
+  }
+  else {
+    return res.status(401).send('Invalid email or password')
+  }
 })
 
 app.get('/questions', function(req, res) {
@@ -71,3 +135,5 @@ app.post("/submissions", function(req, res) {
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
+
+
